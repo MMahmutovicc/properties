a47f620cb86e4b102a4bc5e739b0140b648ca44d
Mirza <mmahmutovi3@etf.unsa.ba>
2025-03-29 10:39:01 +0100

    test

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+console.log('Server port:', PORT);
 
 const FRONTEND_URL = process.env.FRONTEND_URL;
 app.use(cors({
